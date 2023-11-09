61fae539066e8cc96c0ff1702bb45cd4e84d1bfc
Moranoo <12mourad4@gmail.com>
2023-11-09 10:45:13 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyCkedit';
+
+  public Editor = DecoupledEditor;
+  public onReady( editor: DecoupledEditor ): void {
+    const element = editor.ui.getEditableElement()!;
+    const parent = element.parentElement!;
+
+    parent.insertBefore(
+      editor.ui.view.toolbar.element!,
+      element
+    );
+  }
 }
